1cad659096906f57d3f8f8843596772d8eb70484
Rohi Mondal <144237665+Rohimondal04@users.noreply.github.com>
2023-11-04 20:53:08 +0530

    Add files via upload

---
+++ trapezium.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float a,b,c;
+    printf("Enter 1st side : ");
+    scanf("%f",&a);
+    printf("Enter 2nd side : ");
+    scanf("%f",&b);
+    printf("Enter 3rd side : ");
+    scanf("%f",&c);
+    float area=((a+b)/2)*h;
+    printf("Area of the trapezium is : %f\n",area);
+    return 0;
+
+}
