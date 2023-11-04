107485dfc60d42899b5e250a8b8281a87351ccfe
Rohi Mondal <144237665+Rohimondal04@users.noreply.github.com>
2023-11-04 19:42:51 +0530

    Add files via upload

---
+++ rectangle area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float l,b;
+    printf("Enter the length : ");
+    scanf("%f",&l);
+    printf("Enter the breadth : ");
+    scanf("%f",&b);
+    printf(" the area of the recatangle : %f",l*b);
+    return 0;
+}
