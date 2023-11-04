b3b9eb861d92b0bed389284fe1a40b5dc483602d
Rohi Mondal <144237665+Rohimondal04@users.noreply.github.com>
2023-11-04 19:43:23 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float r,
+    printf("Enter the radius of the circle :");
+    scanf("%f",&r);
+    float a=3.14*(r*r);
+    printf("Area of the circle is : %f",a);
+    return 0 ;
+}
