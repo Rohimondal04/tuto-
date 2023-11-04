f7fb9aec065c3e6ba270a627831fae2cf6fe9639
Rohi Mondal <144237665+Rohimondal04@users.noreply.github.com>
2023-11-04 20:52:30 +0530

    Add files via upload

---
+++ vote.c
@@ -0,0 +1,13 @@
+#include<tsdio.h>
+int main(){
+    int age;
+    printf("Enter your age : ");
+    scanf("%d",&age);
+    if (age>=18){
+        printf("You are eligible for vote !");
+    }
+    else{
+        printf("You are not eligible to vote !!");
+    }
+    return 0;
+}
